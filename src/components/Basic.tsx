a3d3e666c9d42cdbbf58963e686e5efd450ab8c6
dawa1004 <wr1004wr1004wr@gmail.com>
2020-10-03 18:05:27 +0900

    add name and discription

---
+++ src/components/Basic.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { TextField } from "@material-ui/core";
+import useStyles from "./styles";
+import { RootState } from "../domain/entity/rootState";
+import { Profile } from "../domain/entity/profile";
+import { PROFILE } from "../domain/services/profile";
+import profileActions from "../store/profile/actions";
+
+const Basic = () => {
+  const dispatch = useDispatch();
+  const profile = useSelector((state: RootState) => state.profile);
+  const classes = useStyles();
+  const handleChange = (member: Partial<Profile>) => {
+    dispatch(profileActions.setProfile(member));
+  };
+
+  return (
+    <>
+      <TextField
+        fullWidth
+        label={PROFILE.NAME}
+        className={classes.formField}
+        value={profile.name}
+        onChange={e => handleChange({ name: e.target.value })}
+      />
+      <TextField
+        fullWidth
+        multiline
+        className={classes.formField}
+        rows={5}
+        label={PROFILE.DESCRIPTION}
+        value={profile.description}
+        onChange={e => handleChange({ description: e.target.value })}
+      />
+    </>
+  );
+};
+
+export default Basic;

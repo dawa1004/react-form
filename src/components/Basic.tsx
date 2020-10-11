d2df804a34be3ff761dc19872506b2453bdb5562
dawa1004 <wr1004wr1004wr@gmail.com>
2020-10-11 18:30:38 +0900

    add gender

--- src/components/Basic.tsx
+++ src/components/Basic.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { TextField } from "@material-ui/core";
+import {
+  TextField,
+  FormControl,
+  FormLabel,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+} from "@material-ui/core";
 import useStyles from "./styles";
 import { RootState } from "../domain/entity/rootState";
 import { Profile } from "../domain/entity/profile";
+import { Gender } from "../domain/entity/gender";
 import { PROFILE } from "../domain/services/profile";
 import profileActions from "../store/profile/actions";
 
@@ -22,7 +30,7 @@ const Basic = () => {
         label={PROFILE.NAME}
         className={classes.formField}
         value={profile.name}
-        onChange={e => handleChange({ name: e.target.value })}
+        onChange={(e) => handleChange({ name: e.target.value })}
       />
       <TextField
         fullWidth
@@ -31,8 +39,26 @@ const Basic = () => {
         rows={5}
         label={PROFILE.DESCRIPTION}
         value={profile.description}
-        onChange={e => handleChange({ description: e.target.value })}
+        onChange={(e) => handleChange({ description: e.target.value })}
       />
+      <FormControl className={classes.formField}>
+        <FormLabel>{PROFILE.GENDER}</FormLabel>
+        <RadioGroup
+          value={profile.gender}
+          onChange={(e) => handleChange({ gender: e.target.value as Gender })}
+        >
+          <FormControlLabel
+            value="male"
+            label="男性"
+            control={<Radio color="primary" />}
+          />
+          <FormControlLabel
+            value="female"
+            label="女性"
+            control={<Radio color="primary" />}
+          />
+        </RadioGroup>
+      </FormControl>
     </>
   );
 };
